092bdaee65967b1408a35df9d790287ff9fbd92c
Vanessa De Oliveira Mello <vanessadeoliveiramello@MacBook-Air-de-Vanessa.local>
2026-08-05 10:52:43 -0300

    Adicionar estrutura base da geração do code challenge e code verifier

--- src/main/java/br/com/vanessacardoso/pkce/internal/Base64Encoder.java
+++ src/main/java/br/com/vanessacardoso/pkce/internal/Base64Encoder.java
@@ -1,4 +1,9 @@
 package br.com.vanessacardoso.pkce.internal;
 
+import java.util.Base64;
+
 public class Base64Encoder {
+    public static String encodeUrlSafe(byte[] data){
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(data);
+    }
 }
